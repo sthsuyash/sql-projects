d07f06ec29e1732e4c28d5dd8788a07aaf267fa8
Suyash Shrestha <sthasuyash11@gmail.com>
2023-03-15 16:43:29 +0545

    add script for info

---
+++ FCC/Student database/student_info.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Info about my computer science students from students database
+
+# chmod +x student_info.sh => run this to give execute permission to the script
+
+echo -e "\n~~ My Computer Science Students ~~\n"
+
